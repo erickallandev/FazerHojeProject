28ee9e42640ceb2c8ffa22fa7a55e5d6692aa0d8
Erick Allan Silva <98674896+erickallandev@users.noreply.github.com>
2022-09-08 11:07:49 -0300

    feat: deploy para o heroku

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const server = express();
 
 const PORT = process.env.PORT || 3001;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+server.use(express.static(path.resolve(__dirname, '../client/build')));
+
 server.get('/api', (req, res) => {
-    res.json({message: "Tudo está funcionando bem!"});
+    res.json({ message: "Tudo está funcionando bem na porta definida!" });
+});
+
+server.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 server.listen(PORT, () => {
