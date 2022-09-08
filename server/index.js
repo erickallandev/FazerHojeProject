454504b0d19cbf37192730c26f65062d3c86d797
Erick Allan Silva <98674896+erickallandev@users.noreply.github.com>
2022-09-08 10:27:43 -0300

    feat: primeira rota criada e funcionando

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const server = express();
 
 const PORT = process.env.PORT || 3001;
 
+server.get('/api', (req, res) => {
+    res.json({message: "Tudo está funcionando bem!"});
+});
+
 server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 })
