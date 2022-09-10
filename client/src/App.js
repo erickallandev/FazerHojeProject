2b2ef8deace8b2798206d1b57c2300f2a14716e1
Erick Allan Silva <98674896+erickallandev@users.noreply.github.com>
2022-09-09 21:41:36 -0300

    feat: add função de excluir atividades

--- client/src/App.js
+++ client/src/App.js
@@ -19,67 +19,81 @@ const App = () => {
         getAtividades();
     }, []);
 
-    const onSubmitForm = async (e) => {
+    const onSubmeterForm = async (e) => {
         e.preventDefault();
         try {
             const body = { atividade: novaAtividade };
-            const response = await fetch("http://localhost:3001/atividades", {
+            await fetch("http://localhost:3001/atividades", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(body)
             });
-            console.log(response);
         } catch (err) {
             console.error(err.message);
         }
     }
 
-    return (
-        <div className="flex flex-col bg-gray-700 box-border min-h-screen justify-start items-center">
-            <div className='w-9/12'>
+    const deletarAtividade = async (id) => {
+        try {
+            await fetch(`http://localhost:3001/atividades/${id}`, {
+                method: "DELETE",
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            });
+            
+        } catch (err) {
+            console.error(err.message);
+        }
+}
 
-                <div className='font-black font-lobster text-3xl md:text-6xl italic text-center text-white tracking-wide mt-8'>O que temos para hoje?</div>
 
-                {/* Adicionar nova atividade */}
+return (
+    <div className="flex flex-col bg-gray-700 box-border min-h-screen justify-start items-center">
+        <div className='w-9/12'>
 
-                <div className='flex mt-20 h-12'>
-                    <form onSubmit={onSubmitForm} className='flex flex-1'>
-                        <input type='text' placeholder='Insira aqui a sua nova atividade' value={novaAtividade} className='flex flex-1 outline-none text-center bg-white rounded-full' onChange={(e) => setNovaAtividade(e.target.value)}></input>
-                        <button className='rounded-full hover:bg-white hover:text-gray-700 bg-gray-700 text-white ml-5 justify-center flex items-center text-sm cursor-pointer p-4 border-2 ease-in duration-200'>Confirmar</button>
-                     </form>
-                </div>
+            <div className='font-black font-lobster text-3xl md:text-6xl italic text-center text-white tracking-wide mt-8'>O que temos para hoje?</div>
 
-                {/* Listar atividades */}
+            {/* Adicionar nova atividade */}
 
-                <div className='flex flex-col mt-8 bg-white text-center'>
-                   <table>
-                        <thead>
-                            <tr>
-                                <th>Status</th>
-                                <th>Atividade</th>
-                                <th>Editar</th>
-                                <th>Deletar</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {listaAtividades.map((item, index) => (
-                                <tr key={index} className='m-8 border border-collapse'>
-                                    <td>
-                                        <input type='checkbox' checked={item.estado}>
-                                        </input>
-                                    </td>
-                                    <td>{item.atividade}</td>
-                                    <td>Editar</td>
-                                    <td>Apagar</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                   </table>
-                </div>
+            <div className='flex mt-20 h-12'>
+                <form onSubmit={onSubmeterForm} className='flex flex-1'>
+                    <input type='text' placeholder='Insira aqui a sua nova atividade' value={novaAtividade} className='flex flex-1 outline-none text-center bg-white rounded-full' onChange={(e) => setNovaAtividade(e.target.value)}></input>
+                    <button className='rounded-full hover:bg-white hover:text-gray-700 bg-gray-700 text-white ml-5 justify-center flex items-center text-sm cursor-pointer p-4 border-2 ease-in duration-200'>Confirmar</button>
+                </form>
             </div>
 
+            {/* Listar atividades */}
+
+            <div className='flex flex-col mt-8 bg-white text-center'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Status</th>
+                            <th>Atividade</th>
+                            <th>Editar</th>
+                            <th>Deletar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {listaAtividades.map((item, index) => (
+                            <tr key={index} className='m-8 border border-collapse'>
+                                <td>
+                                    <input type='checkbox' >
+                                    </input>
+                                </td>
+                                <td>{item.atividade}</td>
+                                <td>Editar</td>
+                                <td onClick={(e) => deletarAtividade(item.id)}>Apagar</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-    )
+
+    </div>
+)
 
 }
 
